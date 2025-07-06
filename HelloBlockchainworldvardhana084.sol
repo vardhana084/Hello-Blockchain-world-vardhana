9f347451787e23130ca580cc93c7382176fa761f
vardhana084 <vardhan0877@gmail.com>
2025-07-07 01:04:11 +0530

    Create HelloBlockchainworldvardhana084.sol

---
+++ HelloBlockchainworldvardhana084.sol
@@ -0,0 +1,8 @@
+/SPDX-License-Identifier:MIT
+pragma solidity ^0.8.30;
+contract HelloBlockchainworldvardhana084 {
+string public greeting = "Hello, Blockchain Vardhana World!";
+function wishgreeting(string memory _greeting) public {
+    greeting = _greeting;
+        }
+}
